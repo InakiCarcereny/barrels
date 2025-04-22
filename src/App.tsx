421f05c41ba5e1675bfe0e1d1039fa0dca0bb407
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-21 21:28:24 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Button, Input } from './components';
+
+import { H1, H2 } from './components-2';
+
+//export { Metodo } from './utils';
+
+//import { Metodo } from './utils'; <-- Lo que viene de metodo si no es usado se elimina (Codigo muerto) pero la importacion se mantiene ya que el bundler no sabe que es lo que hace o si se esta usando (Modulo muerto).
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Button />
+      <Input />
+
+      <H1 />
+      <H2 />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './button'; // <-- Al hacer una exportacion nombrada nos estamos evitando el tree shaking y el codidogo muerto.
+
+//export * from './button'; <-- HAY QUE EVITAR ESTO SI ESTAMOS USANDO LOS BARRELS.
+
+export { Input } from './input';
